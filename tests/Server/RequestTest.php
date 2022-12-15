65647e88c44a1d7379b3f98d67da0519a5868499
Arturo López <lgzarturo@gmail.com>
2022-12-15 09:39:20 -0500

    Pruebas para obtener los parámetros del Request

--- tests/Server/RequestTest.php
+++ tests/Server/RequestTest.php
@@ -3,7 +3,8 @@
 namespace Pandora\Tests\Server;
 
 use Pandora\Constants\HttpMethod;
-use Pandora\Server\Request;
+use Pandora\Exception\NotFoundException;
+use Pandora\Routes\Router;
 use Pandora\Server\ServerMock;
 use PHPUnit\Framework\TestCase;
 
@@ -16,5 +17,51 @@ class RequestTest extends TestCase {
         $request->setBody($body)->setQueryString($queryString);
         $this->assertEquals($queryString, $request->getQueryString());
         $this->assertEquals($body, $request->getBody());
+    }
+    
+    final public function test_data_returns_value_if_key_is_given(): void {
+        $body = ['data' => 'hello', 'content' => 'lorem2'];
+        $server = new ServerMock("/test", HttpMethod::GET);
+        $request = $server->getRequest();
+        $request->setBody($body);
+        $data = $request->getBody('data');
+        $this->assertEquals(['data' => 'hello'], $data);
+    }
+
+    final public function test_query_returns_value_if_key_is_given(): void {
+        $queryString = ['page' => 1, 'offset' => 20, 'order' => 'id,asc'];
+        $server = new ServerMock("/test", HttpMethod::GET);
+        $request = $server->getRequest();
+        $request->setQueryString($queryString);
+        $data = $request->getQueryString('page');
+        $this->assertEquals(['page' => 1], $data);
+    }
+
+    /**
+     * @throws NotFoundException
+     */
+    final public function test_route_parameters_returns_value_if_key_is_given(): void {
+        $router = new Router();
+        $router->get('/test/{id}/data/{param}', static fn () => "test");
+        $server = new ServerMock("/test/1/data/example", HttpMethod::GET);
+        $request = $server->getRequest();
+        $route = $router->resolve($request);
+        $request->setRoute($route);
+        $data = $request->getRouteParams('id');
+        $this->assertEquals(['id' => 1], $data);
+    }
+
+    /**
+     * @throws NotFoundException
+     */
+    final public function test_route_parameters_returns_all_params(): void {
+        $router = new Router();
+        $router->get('/test/{id}/data/{param}', static fn () => "test");
+        $server = new ServerMock("/test/1/data/example", HttpMethod::GET);
+        $request = $server->getRequest();
+        $route = $router->resolve($request);
+        $request->setRoute($route);
+        $data = $request->getRouteParams();
+        $this->assertEquals(['id' => 1, 'param' => 'example'], $data);
     }
 }
